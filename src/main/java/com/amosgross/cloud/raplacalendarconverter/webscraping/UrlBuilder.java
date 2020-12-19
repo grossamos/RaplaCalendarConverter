4ee47330f17b8cf5c9b22779ae6fb606e7f6bba6
amos <email@example.com>
2020-12-19 22:29:46 +0100

    Updated UrlBuilder for a friend in need

--- src/main/java/com/amosgross/cloud/raplacalendarconverter/webscraping/UrlBuilder.java
+++ src/main/java/com/amosgross/cloud/raplacalendarconverter/webscraping/UrlBuilder.java
@@ -6,21 +6,25 @@ public class UrlBuilder {
     private static final String baseUrl = "https://rapla.dhbw-stuttgart.de/rapla";
     private static final String key20D = "txB1FOi5xd1wUJBWuX8lJrog1ZNPBrE7IVuW7j2dZDqY8f6xFPQs3yRuH0p5-aHv";
 
-    public static String getUrl(String courseName, LocalDate dateToSearchFor){
-        String base;
-
-        if (courseName.equals("20D")){
-            base = baseUrl + "?key=" + key20D;
-        }
-        else {
-            throw new IllegalStateException("Hey, we don't support that course yet");
-        }
+    public static String getUrlFromKey(String key, LocalDate dateToSearchFor){
+        String base = baseUrl + "?key="+ key;
 
         base = appendParameter(base, "day", String.valueOf(dateToSearchFor.getDayOfMonth()));
         base = appendParameter(base, "month", String.valueOf(dateToSearchFor.getMonthValue()));
         base = appendParameter(base, "year", String.valueOf(dateToSearchFor.getYear()));
 
         return base;
+    }
+
+    public static String getUrl(String courseName, LocalDate dateToSearchFor){
+
+        if (courseName.equals("20D")){
+            return getUrlFromKey(key20D, dateToSearchFor);
+        }
+        else {
+            throw new IllegalStateException("Hey, we don't support that course yet");
+        }
+
     }
 
     private static String appendParameter(String previousURL, String key, String value){
